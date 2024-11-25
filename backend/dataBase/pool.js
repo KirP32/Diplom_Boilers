b8469268701ec1e18ad19898ca261e249e1259f2
KirP32 <kirillka3232@mail.ru>
2024-11-25 13:25:23 +0300

    changed server ip

--- backend/dataBase/pool.js
+++ backend/dataBase/pool.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost', //185.46.10.111
-    database: 'ADS_Line',
-    password: '123',
-    port: 5432,
-    client_encoding: 'utf8',
+  user: "postgres",
+  host: "185.46.10.111", //185.46.10.111
+  database: "ADS_Line",
+  password: "123",
+  port: 5432,
+  client_encoding: "utf8",
 });
 
 module.exports = pool;
--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,43 +1,53 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const API_URL = `http://localhost:8080` //http://185.46.10.111/api
+export const API_URL = `http://185.46.10.111/api`; //http://185.46.10.111/api
 //http://localhost:8080
 const logged = localStorage.getItem("stay_logged");
 const $api = axios.create({
-    withCredentials: true,
-    baseURL: API_URL
-})
+  withCredentials: true,
+  baseURL: API_URL,
+});
 
 $api.interceptors.request.use((config) => {
-    if (localStorage.getItem('stay_logged') == "false") {
-        config.headers.accessToken = `${sessionStorage.getItem('accessToken')}`
-    }
-    else {
-        config.headers.accessToken = `${localStorage.getItem('accessToken')}`
-    }
+  if (localStorage.getItem("stay_logged") == "false") {
+    config.headers.accessToken = `${sessionStorage.getItem("accessToken")}`;
+  } else {
+    config.headers.accessToken = `${localStorage.getItem("accessToken")}`;
+  }
 
-    config.headers.authorization = 'Bearer $2b$12$IDWkgcBO6qA8xXHovNrejefn9yiDJ4I5OJ4iDcyyNIzFyDeaasnTe';
-    return config;
-})
+  config.headers.authorization =
+    "Bearer $2b$12$IDWkgcBO6qA8xXHovNrejefn9yiDJ4I5OJ4iDcyyNIzFyDeaasnTe";
+  return config;
+});
 
-$api.interceptors.response.use((config) => {
+$api.interceptors.response.use(
+  (config) => {
     return config;
-}, async (error) => {
+  },
+  async (error) => {
     const originalRequest = error.config;
-    if (((error.response.status === 401) && error.config && !error.config._isRetry) && logged == "true") {
-        originalRequest._isRetry = true;
-        // console.log('Пробую выдать авторизацию');
-        try {
-            const response = await axios.get(`${API_URL}/refresh`, { withCredentials: true })
-            localStorage.setItem('accessToken', response.data.accessToken);
-            // console.log('Выдал авторизацию')
-            return $api.request(originalRequest);
-        } catch (e) {
-            console.log('Ошибка авторизации')
-            // console.log(e);
-        }
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry &&
+      logged == "true"
+    ) {
+      originalRequest._isRetry = true;
+      // console.log('Пробую выдать авторизацию');
+      try {
+        const response = await axios.get(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("accessToken", response.data.accessToken);
+        // console.log('Выдал авторизацию')
+        return $api.request(originalRequest);
+      } catch (e) {
+        console.log("Ошибка авторизации");
+        // console.log(e);
+      }
     }
     throw error;
-})
+  }
+);
 
 export default $api;
