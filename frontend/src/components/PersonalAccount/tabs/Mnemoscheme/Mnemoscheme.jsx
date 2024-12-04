d1c6ebc07621edd4a9f86a9cf678541a340a4adf
KirP32 <kirillka3232@mail.ru>
2024-12-04 19:57:53 +0300

    added all leaflet css to Module.scss

--- frontend/src/components/PersonalAccount/tabs/Mnemoscheme/Mnemoscheme.jsx
+++ frontend/src/components/PersonalAccount/tabs/Mnemoscheme/Mnemoscheme.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MapContainer, ImageOverlay } from "react-leaflet";
 //import "leaflet/dist/leaflet.css";
 import "./Mnemoscheme.scss";
+
 export default function Mnemoscheme() {
   const imageSize = 1;
   const gap = 0.1;
