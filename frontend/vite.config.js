0f1c36389eb792484e25ae8e8c6cf2e2b23049a7
KirP32 <kirillka3232@mail.ru>
2024-12-04 20:22:45 +0300

    asd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,21 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.*)$/,
-        replacement: "$1",
-      },
-      { find: "react-leaflet", replacement: "node_modules/react-leaflet" },
-    ],
-  },
-  optimizeDeps: {
-    include: ["react-leaflet", "leaflet"],
-  },
-  build: {
-    rollupOptions: {
-      external: ["react-leaflet", "leaflet"],
-    },
-  },
 });
