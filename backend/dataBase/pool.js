c033ef7f8ed49232bb123e510df681393d63d63e
KirP32 <kirillka3232@mail.ru>
2025-02-06 13:41:28 +0300

    Вернул старые адреса

--- backend/dataBase/pool.js
+++ backend/dataBase/pool.js
@@ -2,8 +2,8 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost", //185.46.10.111
-  database: "ADS_Line", //
+  host: "172.17.0.1", //185.46.10.111
+  database: "ADS_Line", // 172.17.0.1
   password: "123",
   port: 5432,
   client_encoding: "utf8",
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = "wss://ads-line.pro/api";
 
 export const socket = io(URL, {
+  transports: ["websocket"],
   autoConnect: false,
 });
