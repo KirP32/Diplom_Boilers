dc7ecf59c12d9fdc687ee51e4f93f2a4d3d91e5a
KirP32 <kirillka3232@mail.ru>
2025-02-18 16:12:54 +0300

    Fix: изменён nginx для api/socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = "https://ads-line.pro";
+const URL = "https://ads-line.pro"; // Мне кажется нужно добавить на /api/socket.io"
 
 export const socket = io(URL, {
   path: "/api/socket.io",
-  transports: ["websocket", "polling"],
+  transports: ["websocket"],
   autoConnect: false,
   secure: true,
 });
