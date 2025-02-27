86fe43936de497e6cdd563695ac72a72ce95b6fb
KirP32 <kirillka3232@mail.ru>
2025-02-27 13:28:49 +0300

    Fix: Возвращены старые адреса для сервера

--- backend/dataBase/pool.js
+++ backend/dataBase/pool.js
@@ -2,8 +2,8 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost", //185.46.10.111
-  database: "ADS_Line", // 172.17.0.1
+  host: "172.17.0.1", //185.46.10.111 172.17.0.1
+  database: "ADS_Line",
   password: "123",
   port: 5432,
   client_encoding: "utf8",
