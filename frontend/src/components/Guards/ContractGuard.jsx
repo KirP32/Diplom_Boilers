f3261e2ff3b2a15ecfa4e58da1802454ec458d61
KirP32 <kirillka3232@mail.ru>
2025-03-21 17:00:13 +0300

    Fix: возможно устранена проблема с ошибкой token must be a string

--- frontend/src/components/Guards/ContractGuard.jsx
+++ frontend/src/components/Guards/ContractGuard.jsx
@@ -6,7 +6,8 @@ import { jwtDecode } from "jwt-decode";
 const ContractGuard = ({ children }) => {
   const token =
     sessionStorage.getItem("accessToken") ||
-    localStorage.getItem("accessToken");
+    localStorage.getItem("accessToken") ||
+    "";
   if (jwtDecode(token).access_level === 1) {
     return children;
   }
