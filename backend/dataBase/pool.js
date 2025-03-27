f6ce131654785750e90b054b880f8038e8cdd9ff
KirP32 <kirillka3232@mail.ru>
2025-03-27 15:02:26 +0300

    Fix: адреса...

--- backend/dataBase/pool.js
+++ backend/dataBase/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost", //185.46.10.111 172.17.0.1
+  host: "172.17.0.1", //185.46.10.111 172.17.0.1
   database: "ADS_Line",
   password: "123",
   port: 5432,
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
-
+const URL = "https://ads-line.pro";
+// http://localhost:8080
 export const socket = io(URL, {
   path: "/api/socket.io",
   transports: ["websocket"],
   autoConnect: false,
-  //secure: true,
+  secure: true,
 });
