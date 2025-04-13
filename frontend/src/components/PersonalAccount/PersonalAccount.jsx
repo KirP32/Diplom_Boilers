ab8c2fa5cad39051d268a34e770cdb2d3866459c
KirP32 <kirillka3232@mail.ru>
2025-04-13 20:32:41 +0300

    Fix: Проверка стилей

--- frontend/src/components/PersonalAccount/PersonalAccount.jsx
+++ frontend/src/components/PersonalAccount/PersonalAccount.jsx
@@ -111,6 +111,7 @@ export default function PersonalAccount() {
                 : "604px",
           }}
         >
+          <></>
           <img></img>
           {devicesArray && (
             <>
